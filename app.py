06d7ac3c6ad1900fd5e3f0da96d603ec8c958b9b
tmacinc <tmack090@gmail.com>
2021-01-29 14:14:05 -0500

    added working background thread and moved daq interacts here. Route functions now work with variables dictionary

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from threading import Thread, Event, Lock
 from queue import Queue, Empty
 from flask_socketio import SocketIO, emit
 import json
+from time import sleep
+from datetime import datetime, timedelta
 #import eventlet
 from waitress import serve
 
@@ -15,6 +17,19 @@ with open('config.txt') as json_file:
     settings = json.load(json_file)
 #socketio = SocketIO(app)
 daq = daq.dataforth(settings)
+
+variables = {}
+variables["daq_channels"] = []
+variables["vars_raw"] = {}
+for key in settings["channel_map_inputs"]:
+    variables['daq_channels'].append(key)
+for key in settings["channel_map_outputs"]:
+    variables['daq_channels'].append(key)
+for channel in variables['daq_channels']:
+    variables["vars_raw"][channel] = 0
+
+
+#------------------- Route Functions - Perform task when browser directs to link (serve html etc) ---------------------
 
 @app.route('/')
 def index():
@@ -31,7 +46,9 @@ def permeation():
 @app.route('/_update_page_data') #Accepts requested variables when page is loaded and sends current values to page. Could also be used to keep track of what is needed in the back end.
 def update_page_data():
     channels_requested = list(request.args.to_dict().keys())
-    data = daq.read_channels(channels_requested) #if using variable layer to background control task, switch this to update variable and have background task call this function.
+    data = {}
+    for channel in channels_requested:
+        data[channel] = variables['vars_raw'][channel]
     return jsonify(ajax_data=data)
 
 @app.route('/_set_control') #Accepts requested control variable from user and sends value to controller.
@@ -40,21 +57,47 @@ def set_control():
     channels = list(request.args.to_dict().keys())
     channel_name = channels[0]
     print("Received request to update setting: " + channel_name + " to new value: " + msg[channel_name])
-    daq.write_channels(msg) #if using variable layer to background control task, switch this to update variable and have background task call this function.
+    queue.put({"write_channels": msg})
     return jsonify(ajax_response="Received channel -> value: " + str(channel_name) + " -> " + str(msg[channel_name]))
 
 @app.route('/_maq20_fetch_data') #Used for maq20_overview.html
 def maq20_fetch_data():
     data = daq.read_modules(daq.modules)
-    print(data)
     return jsonify(ajax_data=data)
 
-def background_tasks(variables, queue=Queue):
+#--------------------- Regular functions - Can be used by routes, background thread etc. -----------------------
 
-    pass
+def read_daq(): # get current channel values from list in variables['vars_raw']
+    channels = variables['daq_channels']
+    data = daq.read_channels(channels)
+    update_variables(data)
 
-#queue = Queue()
-#background = Thread(target=background_tasks, args=(variables, queue))
+def update_variables(data):
+    for key in data.keys():
+        variables['vars_raw'][key] = data[key]
+
+def background_tasks(queue=Queue): # Used for managing daq, control functions etc. Will run without client connected.
+    print("Background thread started")
+    t_now = datetime.now()
+    t_start = t_now
+    t_next = t_now + timedelta(seconds=1)
+    while True:
+        while t_now < t_next:                               # runs at higher frequency
+            if not queue.empty(): # process queue
+                task = queue.get()
+                for key in task.keys():
+                    if key == "write_channels":
+                        daq.write_channels(task[key])
+            t_now = datetime.now()
+            sleep(0.01)
+        t_next = t_next + timedelta(seconds=1)              # runs every 1 second
+        t_now = datetime.now()
+        read_daq()
+
+queue = Queue()
+background = Thread(target=background_tasks, args=(queue,))
+background.daemon = True
+background.start()
 
 
 if __name__ == '__main__':
