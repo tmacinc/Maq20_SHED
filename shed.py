2e6ee868b947ecad8a33e05dd368eacbf4e4a0de
tmacinc <tmack090@gmail.com>
2021-02-11 09:21:37 -0500

    starting working on shed class

---
+++ shed.py
@@ -0,0 +1,15 @@
+
+
+class shed():
+
+    def __init__(self, name, settings):
+        self.name = name
+        self.settings = settings
+        self.state = "off"
+        self.configs = settings['states_available']
+
+    def change_state(self, mode):
+        # exit current mode
+        # enter new mode
+
+    
